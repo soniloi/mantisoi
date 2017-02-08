680f532a3a5cc10d78d248d99132ae6b1e2fc632
soniloi <niloinso@tcd.ie>
2017-02-08 22:02:59 +0000

    Split functions in TextParser

--- mantisoi/TextParser.py
+++ mantisoi/TextParser.py
@@ -2,26 +2,35 @@
 
 class TextParser:
 
+    HEADER_START = "="
+
     @staticmethod
     def parse_text(text):
 
-        # Find the extent of the intro section
         lines = text.split("\n")
+        intro, section_start_index = TextParser.parse_intro(lines)
+        sections = TextParser.parse_sections(lines, section_start_index)
+        return intro, sections
+
+    @staticmethod
+    def parse_intro(lines):
         intro = ""
         section_start_index = 0
-        while section_start_index < len(lines) and not lines[section_start_index].startswith("="):
+        while section_start_index < len(lines) and not lines[section_start_index].startswith(TextParser.HEADER_START):
             intro = intro + lines[section_start_index]
             section_start_index += 1
+        return intro, section_start_index
 
-        # Split the remaining sections
+    @staticmethod
+    def parse_sections(lines, section_start_index):
+
         sections = []
         current_section = lines[section_start_index]
         for line in lines[section_start_index:]:
-            if line.startswith("="):
+            if line.startswith(TextParser.HEADER_START):
                 sections.append(current_section)
                 current_section = ""
             current_section = current_section + line
 
-        # Append the last section
-        sections.append(current_section)
-        return intro, sections
+        sections.append(current_section) # Append the last section
+        return sections
