a4ab61787763910a516e940d4b0689d63ee92213
soniloi <niloinso@tcd.ie>
2017-02-15 23:27:59 +0000

    Added ability to split content from its references

---
+++ mantisoi/ContentParser.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import re
+
+class ContentParser:
+
+    REF_START = "<ref>"
+    REF_END = "</ref>"
+    REF_PATTERN = REF_START + ".*?" + REF_END
+
+    @staticmethod
+    def parse_content(content):
+
+        refs = []
+        ref_matches = re.findall(ContentParser.REF_PATTERN, content)
+        for ref_match in ref_matches:
+            refs.append(ref_match[len(ContentParser.REF_START):len(ref_match) - len(ContentParser.REF_END)])
+
+        unrefed_content = re.sub(ContentParser.REF_PATTERN, "", content)
+
+        return unrefed_content, refs
