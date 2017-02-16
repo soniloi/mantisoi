ab32d701d398b9aa52a0cc19f74c5f73fba132ce
soniloi <niloinso@tcd.ie>
2017-02-16 20:58:38 +0000

    Added parsing for reference lists

--- mantisoi/ContentParser.py
+++ mantisoi/ContentParser.py
@@ -7,6 +7,7 @@ class ContentParser:
     REF_START = "<ref>"
     REF_END = "</ref>"
     REF_PATTERN = re.compile(REF_START + ".*?" + REF_END, re.MULTILINE|re.DOTALL)
+    REF_LIST_ELEMENT_START = re.compile("^\* +")
 
     @staticmethod
     def parse_content(content):
@@ -14,7 +15,11 @@ class ContentParser:
         refs = []
         ref_matches = re.findall(ContentParser.REF_PATTERN, content)
         for ref_match in ref_matches:
-            refs.append(ref_match[len(ContentParser.REF_START):len(ref_match) - len(ContentParser.REF_END)])
+            ref_match_untagged = ref_match[len(ContentParser.REF_START):len(ref_match) - len(ContentParser.REF_END)]
+            ref_elements = ref_match_untagged.splitlines()
+            for ref_element in ref_elements:
+                ref = re.sub(ContentParser.REF_LIST_ELEMENT_START, "", ref_element)
+                refs.append(ref)
 
         unrefed_content = re.sub(ContentParser.REF_PATTERN, "", content)
 
