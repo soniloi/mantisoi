937706361e6448954fb3d4bbe51b3a89ef80783c
soniloi <niloinso@tcd.ie>
2017-02-21 22:34:38 +0000

    Fixed issue with non-final parenthesized terms

--- generator/TitleSplitter.py
+++ generator/TitleSplitter.py
@@ -4,7 +4,7 @@ import re
 
 class TitleSplitter:
 
-    POST_PATTERN = re.compile("\(.+?\)")
+    POST_PATTERN = re.compile("\(.+?\) *")
 
     @staticmethod
     def split(title_str):
@@ -22,15 +22,18 @@ class TitleSplitter:
     def get_post(title_str):
 
         post = []
-        post_str_index = len(title_str)
+        post_str_start_index = len(title_str)
         post_strs = list(TitleSplitter.POST_PATTERN.finditer(title_str))
 
-        if post_strs and len(post_strs) > 0:
-            post_str_index = post_strs[-1].start()
-            post_str = title_str[post_str_index+1:-1] # Strip the parentheses
-            post = post_str.split()
+        if post_strs:
+            post_str_end_index = post_strs[-1].end()
 
-        return post, post_str_index
+            if post_str_end_index == len(title_str):
+                post_str_start_index = post_strs[-1].start()
+                post_str = title_str[post_str_start_index+1:-1] # Strip the parentheses
+                post = post_str.split()
+
+        return post, post_str_start_index
 
 
     @staticmethod
