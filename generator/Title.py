59cb700177cb421ddf4c93844be9c6302d28e0b3
soniloi <niloinso@tcd.ie>
2017-02-22 22:38:02 +0000

    Added basic child title generation

--- generator/Title.py
+++ generator/Title.py
@@ -7,6 +7,17 @@ class Title:
         self.core = core
         self.post = post
 
+    def generate_new(self, other):
+        if self.pre:
+            return Title(self.pre, other.core, other.post)
+        if other.pre:
+            return Title(other.pre, self.core, self.post)
+        if self.post:
+            return Title(other.pre, other.core, self.post)
+        if other.post:
+            return Title(self.pre, self.core, other.post)
+        return Title(self.pre, self.core, self.post)
+
     def write_out(self):
         print self.pre,
         print self.core,
